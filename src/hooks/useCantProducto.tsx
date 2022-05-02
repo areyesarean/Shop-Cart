81e976ee46353ee66060139aa7260bbb89857819
areyesarean <alereyarean@gmail.com>
2022-05-02 18:09:42 +0200

    hook useCantProd

---
+++ src/hooks/useCantProducto.tsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+
+const useCantProducto = () => {
+  const [cant, setCant] = useState(0);
+  const [totalPagar, setTotalPagar] = useState(0);
+  const cart: any = useSelector<any>((state) => state.shop.cart);
+
+  useEffect(() => {
+    setTotalPagar(0);
+    setCant(0);
+    cart.map((prod: any) => {
+      setTotalPagar((total) => (total += prod.price * prod.cantSelect));
+      setCant((cant) => (cant += prod.cantSelect));
+    });
+  }, [cart]);
+
+  return {
+    cantProd: cant,
+    totalPagar: totalPagar.toFixed(2),
+    cartIsEmpty: cart.length === 0,
+  };
+};
+
+export default useCantProducto;
